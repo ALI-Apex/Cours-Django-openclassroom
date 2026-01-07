23a608081146ef7349ed234dab5e2a3d26794f97
ali <amadouali0120@gmail.com>
2026-01-07 23:28:32 +0000

    fix(authentication): add custom User model with profile photo and role

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    CREATOR = "CREATOR"
+    SUBSCRIBER = "SUBSCRIBER"
+
+    ROLE_CHOICES = ((CREATOR, "Createur"), (SUBSCRIBER, "Abonne"))
+
+    profile_photo = models.ImageField(verbose_name="photo de profil")
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name="Role")
